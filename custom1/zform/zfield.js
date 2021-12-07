37481f31d9ca09eeb2fb0a520a1d93d071204f6f
baichao ling <1585638808@qq.com>
2021-12-07 16:58:37 +0800

    支持了cm-field根据form配置改变

--- custom1/zform/zfield.js
+++ custom1/zform/zfield.js
@@ -5,6 +5,7 @@ import bform__behavior from './bform__behavior'
 export function createFieldComponent() {
   Component({
     behaviors: [bform__behavior, computedBehavior],
+    // externalClasses: ['class'],
     relations: {
       'bform__behavior': {
         type: 'ancestor', // 关联的目标节点应为祖先节点
@@ -48,10 +49,12 @@ export function createFieldComponent() {
     lifetimes: {
       ready() {
         let config = this.zform__getFieldConfig(this.data.formId,  this.data.configPath);
-        console.log(config)
+        let form = this.getFormRef();
+        // console.log(form.zformi_formWidgetConfig());
         this.setData({
           fieldPath: this.zform__getObjPathFromPathArr(this.data.pathArr),
-          uiConfig: config?.ui ?? {}
+          uiConfig: config?.ui ?? {},
+          formWigetConfig: form.zformi_formWidgetConfig()
         })
       }
     },
--- custom1/zform/zform.js
+++ custom1/zform/zform.js
@@ -8,6 +8,9 @@ export function createFormComponent({
 } = {}) {
   Component({
     behaviors: [bform__behavior, computedBehavior],
+    options: {
+      styleIsolation: 'apply-shared'
+    },
     relations: {
       'bform__behavior': {
         type: 'descendant', // 关联的目标节点应为子孙节点
@@ -32,7 +35,8 @@ export function createFormComponent({
       // ],
       model_str: '',
       model: {},
-
+      formConfig: {},
+      formUIConfig: {}
     },
     watch: {
       model_str: function(newVal) {
@@ -45,6 +49,12 @@ export function createFormComponent({
     lifetimes: {
       created() {
         this.registerForm(this.data.uuid, this);
+        let formUIConfig = formDef?.def?.ui ?? {}
+        this.setData({
+          formConfig: formDef?.def,
+          formUIConfig: formUIConfig,
+          formWidgetConfig: Object.fromEntries(formUIConfig.attrs)
+        })
         console.log('form created', this.data)
       },
       ready() {
@@ -53,6 +63,9 @@ export function createFormComponent({
     },
   
     methods: {
+      zformi_formWidgetConfig() {
+        return this.data.formWidgetConfig
+      },
       getFormConfig() {
         return formDef
       },
